fc4d948a143286bc84adf1454e8b6918f90b00e9
nikhilthomas112 <nikhilthomas112@gmail.com>
2021-11-15 21:32:16 +0530

    UI components are almost completed.

--- app/src/main/java/com/controller/thecars/MainActivity.kt
+++ app/src/main/java/com/controller/thecars/MainActivity.kt
@@ -11,15 +11,17 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.controller.thecars.ui.theme.GlassBlack
-import com.controller.thecars.ui.theme.Shapes
+import com.controller.thecars.ui.theme.GlassWhite
 import com.controller.thecars.ui.theme.TheCarsTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +29,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheCarsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    BaseLayout()
+                Box {
+                    Image(
+                        painter = painterResource(id = R.drawable.bg_gtr),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 250.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Surface(color = Color.Transparent,modifier = Modifier.fillMaxSize()) {
+                        BaseLayout()
+                    }
                 }
+
             }
         }
     }
@@ -57,22 +70,22 @@ fun DefaultPreview() {
 fun UpDownBtns(btnName: String) {
     Column(
         modifier = Modifier
-            .width(150.dp)
-            .padding(30.dp)
+            .width(170.dp)
+            .padding(horizontal = 30.dp, vertical = 20.dp)
             .background(Color.Transparent)
     ) {
 
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier
-                .size(90.dp, 100.dp)
+                .size(110.dp, 60.dp)
                 .background(Color.Transparent),
             shape = RoundedCornerShape(15.dp)
         ) {
             Image(
                 painter = painterResource(R.drawable.up_arrow),
                 contentDescription = "$btnName window up button",
-                modifier = Modifier.size(50.dp)
+                modifier = Modifier.size(30.dp)
             )
         }
 
@@ -85,14 +98,14 @@ fun UpDownBtns(btnName: String) {
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier
-                .size(90.dp, 100.dp)
+                .size(110.dp, 60.dp)
                 .background(Color.Transparent),
             shape = RoundedCornerShape(15.dp)
         ) {
             Image(
                 painter = painterResource(R.drawable.down_arrow),
                 contentDescription = "$btnName window down button",
-                modifier = Modifier.size(50.dp)
+                modifier = Modifier.size(30.dp)
             )
         }
 
@@ -104,7 +117,7 @@ fun UpDownBtns(btnName: String) {
 
         Text(
             text = btnName,
-            color = Color.White,
+            color = Color.Black,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
@@ -122,7 +135,7 @@ fun WindowBtns() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(GlassBlack),
+                .background(GlassWhite),
             horizontalArrangement = Arrangement.Center
         ) {
             UpDownBtns(btnName = "Front Left")
@@ -131,7 +144,7 @@ fun WindowBtns() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(GlassBlack),
+                .background(GlassWhite),
             horizontalArrangement = Arrangement.Center
         ) {
             UpDownBtns(btnName = "Rear Left")
@@ -142,23 +155,64 @@ fun WindowBtns() {
 
 
 @Composable
-fun BaseLayout() {
-    Column {
-        Text(
-            text = "Controls",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 20.dp,bottom = 15.dp),
-            color = Color.Black,
-            fontSize = 60.sp
-        )
+fun HeaderImage() {
 
-        Card(
+    Card(
+        shape = RoundedCornerShape(0.dp, 0.dp, 20.dp, 20.dp)
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(R.drawable.gtr),
+                contentDescription = "Image of a Nissan GTR"
+            )
+        }
+        Text(
+            text = "Nissan GTR",
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp,
+            color = Color.Black,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp)
-        ) {
-            WindowBtns()
+                .padding(15.dp)
+        )
+    }
+}
+
+
+@Composable
+fun BaseLayout() {
+
+    Column(
+        modifier = Modifier
+            .background(Color.Transparent)
+    ) {
+        HeaderImage()
+
+        Column {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 20.dp, 20.dp, 0.dp),
+                backgroundColor = GlassWhite
+            ) {
+                Text(
+                    text = "Window Controls",
+                    color = Color.Black,
+                    fontSize = 30.sp,
+                    modifier = Modifier
+                        .background(GlassWhite)
+                        .padding(15.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                backgroundColor = GlassWhite
+            ) {
+                WindowBtns()
+            }
         }
         
     }
